7580fc153685e3dda3f88642b22ddb2f906035cc
Matheusmelo001 <matheusfferreira081@gmail.com>
2026-05-27 00:29:59 +0000

    Meu primeiro código react

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { Search, user } from "lucide-react";
+
+export function Header() {
+    return (
+        <header className="header-area"
+            style={{
+                display: 'flex',
+
+                justifyContent: 'space-between',
+                aligntems: 'center',
+                padding: '0 30px',
+                backgroundcolor: '#5375c4',
+                borderBottom: '1px solid #fff'
+            }}>
+
+
+
+        </header>
+    )
+}
+
+<><div style={{
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px'
+}} /><Search size={20} color="#fff"/>
+<input
+type="text"
+placeholder="Pesquisar nop GrauHUB..."
+style={{background: 'none'
+    border: 'none',
+    outline: 'nopne'
+}}
+></div></>
