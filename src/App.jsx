b7cf7942b795fa513f38ad33ace8fcde13a9e110
alisonmelo <61011223+alisonmelo@users.noreply.github.com>
2026-05-28 23:46:19 +0000

    update: codigo atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import  {Header} from './components/Header'
+import { StudentCard } from './components/StudentCard'
 function App() {
   
   return (
@@ -9,9 +10,16 @@ function App() {
       <aside className='sidebar-area'>
         <h2 style={{padding:'20px'}}>GrauConnect</h2>
       </aside>
-      <main className='main-area'>
-        <h1>Dashboard da Turma</h1>
+      <main className='main-area' style={{color: 'white'}}>
+        <h1 style={{color:'white'}}>Dashboard da Turma</h1>
         <p>Aguardando componentes dos alunos</p>
+        <section style={{
+          display:'grid',
+          gridTemplateColumns:'repeat(auto-fill, minmax(320px,1fr))',
+          gap:'20px'
+        }}>
+          <StudentCard/>
+        </section>
       </main>
       <aside className='aside-area'>
         <h3 style={{padding:'20px'}}>Notificações</h3>
---
+++ src/data/alunos.js
@@ -0,0 +1,23 @@
+export const alunos = [
+    {
+        id: 1,
+        nome: 'aluno lindo de jesui',
+        stack: 'tech lead/ full stack',
+        bio: 'A vida só é dura para quem é mole',
+        foto: '#',
+        github: '#',
+        linkedin: '#',
+        portfolio: '#'
+    },
+    {
+        id: 2,
+        nome: 'aluno lindo de jesui',
+        stack: 'tech lead/ full stack',
+        bio: 'A vida só é dura para quem é mole',
+        foto: '#',
+        github: '#',
+        linkedin: '#',
+        portfolio: '#'
+    }
+
+];
