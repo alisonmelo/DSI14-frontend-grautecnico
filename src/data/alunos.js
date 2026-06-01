b9c2720c9679d167f4fe202bfebb273134c68993
infenexzero <infenexzero@gmail.com>
2026-06-01 22:53:22 +0000

    feat: adicionado dados do aluno x

--- src/data/alunos.js
+++ src/data/alunos.js
@@ -20,6 +20,16 @@ export const alunos = [
     github: "#",
     linkedin: "#",
     portfolio: "#"
+  },
+  {
+    id: 3,
+    nome: "Dream Foox",
+    stack: "  Back-end Dev",
+    bio: "Programador Junior Do ilan maiskI ",
+    foto: "https://www.estadao.com.br/resizer/v2/URY27IBCVBFWDHQYVWPOW3WE7M.jpg?quality=80&auth=5ea63a5fc4568766a8b294ca562294ada9306a2faec80558b0e3156e8748367f&width=380", // Foto fake bonita
+    github: "https://github.com/settings/profile",
+    linkedin: "https://github.com/settings/profile",
+    portfolio: "https://github.com/settings/profile"
   }
   // Crie o seu depois daqui...
 ];
