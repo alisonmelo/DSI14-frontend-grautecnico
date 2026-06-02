3700b258420dcd0864b488edbab8656833cec273
robertatila <robertatila154@gmail.com>
2026-06-02 22:26:30 +0000

    correção da pullrequest removendo arquivos desnecessarios

--- src/data/alunos.js
+++ src/data/alunos.js
@@ -27,7 +27,7 @@ export const alunos = [
     nome: "Robert(Aluno)",
     stack: "Full Stack / CyberCode",
     bio: "Estudando para ser o melhor do purple team",
-    foto: "https://br.pinterest.com/pin/1089237859964253727/", // Foto fake bonita
+    foto: "https://i.pinimg.com/736x/93/88/a6/9388a6238562da0b556a3974b78ef77d.jpg", // Foto fake bonita
     github: "github.com/robertatila",
     linkedin: "https://linkedin.com/in/robertatila",
     portfolio: "https://Robert.dev"
